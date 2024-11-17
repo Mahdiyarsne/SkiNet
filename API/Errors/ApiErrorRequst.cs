349d566963a2fba63b0cd2ec5d71a6eb131cbc51
Mahdiyar Sne <mahdiyarsne98@Gmail.com>
2024-11-17 15:01:02 +0330

    مدیرت خطا اتمام شد

---
+++ API/Errors/ApiErrorRequst.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Errors
+{
+	public class ApiErrorRequst (int statusCode ,string message , string? detalis)
+	{
+        public int StatusCode { get; set; } = statusCode;
+
+        public string Message { get; set; } = message;
+
+        public string? Detalis { get; set; } = detalis;
+    }
+}
