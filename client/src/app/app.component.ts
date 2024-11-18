fd79541845a1375004f18375f90456e0d08b3298
Mahdiyar Sne <mahdiyarsne98@Gmail.com>
2024-11-18 22:01:06 +0330

    کامپونت هدر اتمام شد

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,15 +1,26 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  baseUrl = 'https://localhost:5001/api/';
+  private http = inject(HttpClient);
   title = 'SkiNet';
-  http = inject(HttpClient);
+  products: any[] = [];
+
+  ngOnInit(): void {
+    this.http.get<any>(this.baseUrl + 'products').subscribe({
+      next: (response) => this.products = response.data,
+      error: (error) => console.log(error),
+      complete: () => console.log('complete'),
+    });
+  }
 }
