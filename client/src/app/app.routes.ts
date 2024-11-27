1c72a24e566b96587554749d862cfd9facde768b
Mahdiyar Sne <mahdiyarsne98@Gmail.com>
2024-11-27 14:22:58 +0330

    کامپونت چک اوت اضافه شد

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -6,10 +6,12 @@ import { TestErrorComponent } from './features/test-error/test-error.component';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 import { ServerErrorComponent } from './shared/components/server-error/server-error.component';
 import { CartComponent } from './features/cart/cart.component';
+import { CheckoutComponent } from './features/checkout/checkout.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'checkout', component: CheckoutComponent },
   { path: 'shop', component: ShopComponent },
   { path: 'error', component: TestErrorComponent },
   { path: 'not-found', component: NotFoundComponent },
