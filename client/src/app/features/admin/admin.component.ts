58c3293d887a6ef1bf57bee51eb4ce4c4da5cbfd
Mahdiyar Sne <mahdiyarsne98@Gmail.com>
2024-12-28 01:49:48 +0330

    کامپونت ائمین اتمام شد

--- client/src/app/features/admin/admin.component.ts
+++ client/src/app/features/admin/admin.component.ts
@@ -7,33 +7,40 @@ import {
 } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { Order } from '../../shared/models/order';
-import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import {
+  MatPaginator,
+  MatPaginatorModule,
+  PageEvent,
+} from '@angular/material/paginator';
 import { AdminService } from '../../core/services/admin.service';
 import { OrderParams } from '../../shared/models/orderParams';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
-import { MatLabel, MatSelectChange, MatSelectModule } from '@angular/material/select';
+import {
+  MatLabel,
+  MatSelectChange,
+  MatSelectModule,
+} from '@angular/material/select';
 import { CurrencyPipe, DatePipe } from '@angular/common';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import{MatTabsModule} from '@angular/material/tabs'
-import {RouterLink } from '@angular/router';
-
-
+import { MatTabsModule } from '@angular/material/tabs';
+import { RouterLink } from '@angular/router';
+import { DialogService } from '../../core/services/dialog.service';
 
 @Component({
   selector: 'app-admin',
   imports: [
     MatTableModule,
-    MatPaginatorModule,  
-    MatButton, 
+    MatPaginatorModule,
+    MatButton,
     MatIcon,
     MatSelectModule,
     DatePipe,
     CurrencyPipe,
     MatLabel,
-      RouterLink,
+    RouterLink,
     MatTooltipModule,
-    MatTabsModule
+    MatTabsModule,
   ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss',
@@ -43,12 +50,13 @@ export class AdminComponent implements AfterViewInit, OnInit {
     'id',
     'buyerEmail',
     'orderDate',
-    "total",
+    'total',
     'status',
     'action',
   ];
   dataSource = new MatTableDataSource<Order>([]);
   private adminService = inject(AdminService);
+  private dialogService = inject(DialogService);
   orderParams = new OrderParams();
   totalItems = 0;
   statusOptions = [
@@ -88,5 +96,24 @@ export class AdminComponent implements AfterViewInit, OnInit {
     this.orderParams.filter = event.value;
     this.orderParams.pageSize = 1;
     this.loadOrders();
+  }
+
+  async openConfirmDialog(id : number){
+    const confirmed = await this.dialogService.confirm(
+      'Confirm refund',
+       'Are you sure about this?'
+    )
+
+    if(confirmed) this.refundOrder(id);
+  }
+
+  refundOrder(id: number) {
+    this.adminService.refundOrder(id).subscribe({
+      next: (order) => {
+        this.dataSource.data = this.dataSource.data.map((o) =>
+          o.id === id ? order : o
+        );
+      },
+    });
   }
 }
